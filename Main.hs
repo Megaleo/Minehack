99ecdde66751a84f412ae7e0608dbd3a5b901b07
root <root@localhost.localdomain>
2013-05-28 21:03:59 -0300

    Initial Commit with imports

---
+++ Main.hs
@@ -0,0 +1,6 @@
+module Main where
+
+import System.Console.ANSI
+import System.IO
+import System.Random 
+import Control.Monad
---
+++ World.hs
@@ -0,0 +1,6 @@
+module World where
+
+import System.Console.ANSI
+import System.IO
+import System.Random 
+import Control.Monad
