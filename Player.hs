60e65158a81300a618605a63be3481c375fa1ff0
root <root@localhost.localdomain>
2013-05-29 22:37:05 -0300

    Setting up "Block", "Item", "Player" and "Tiles" files with some usefull packages

---
+++ Player.hs
@@ -0,0 +1,7 @@
+module Player where
+
+import System.Console.ANSI
+import System.IO
+import System.Random 
+import Control.Monad
+import Data.Array
---
+++ Tile.hs
@@ -0,0 +1,7 @@
+module Tile where
+
+import System.Console.ANSI
+import System.IO
+import System.Random 
+import Control.Monad
+import Data.Array
