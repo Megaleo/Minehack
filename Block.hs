4a4f0212e0d8a0be81574406af9f44ce95f23afd
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-02 20:46:54 -0300

    Adding classes Block and Item

--- Block.hs
+++ Block.hs
@@ -1,7 +1,39 @@
 module Block where
+-- "Block.RawMaterial" for block's raw materials (There are blocks!) 
+--
+-- Here will be all the definitions and functions around the concept of the Block.
+--
+-- #@TODO@#
+--
+-- @Substitute some "Int" with "Roll from to n-times".
+
 
 import System.Console.ANSI
 import System.IO
 import System.Random 
 import Control.Monad
 import Data.Array
+
+import qualified Tile as T
+
+-- Caracteristics to be an Block (class or data?)
+class T.TileType a => Block a where
+
+--Block's Visibility 
+  seeThrough  :: a -> Bool --seeThrough :: a -> ColorSpectrum -> Bool       
+
+--Block's "Walk and stand over" caracteristic (with damage value)                    
+  walkThrough :: a -> Int -> Bool  
+
+--Block's Fixed amount of hits to break (Initial damage value)		    	         
+  hitstoBreak :: a -> Int  
+
+--Block's Weight		    	         
+  weight      :: a -> Int -- weight :: a -> a -> Weight         
+				           
+--Block's drop when broken				           
+  drops       :: (T.TileType b) => a -> Maybe [b]          
+                             
+--Block's physical material
+--material    :: a -> [Block.RawMaterial]
+                            
--- Item.hs
+++ Item.hs
@@ -1,7 +1,31 @@
 module Item where
+-- "Item.Money" for only money representention items 
+--
+-- Here will be all the definitions and functions around the concept of the Item.
+--
+-- Every Item can be stored in a slot of a container (chest, furnace, player's inventory, a box, etc.) 
+--
+-- An Item should be normally pickable by the player, but it couldn't be due
+-- to the weight or other natural caracteristic of the item.
+--
+-- There will be many more other others modules and classes to specify the big "Item" class, 
+
 
 import System.Console.ANSI
 import System.IO
 import System.Random 
 import Control.Monad
 import Data.Array
+
+import qualified Tile as T
+
+--Caracteristics to be an Item
+class T.TileType a => Item a where  
+
+--Item's Maximum amount of itself that can be stacked in one 
+--slot on any container 
+  maxStack  :: a -> Int   
+ 
+--Item's Weight			                           
+  weight    :: a -> Int    	 
+
