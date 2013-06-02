cc398e6086a6f66ef0e28f899da44d267f34ff3e
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-02 20:45:37 -0300

    Adding classes TileType, Attribute and the Tile

---
+++ Attribute.hs
@@ -0,0 +1,33 @@
+module Attribute where
+-- All the attributes given to a Tile (TileType) by something or someone in-game
+--
+-- #@ Examples @#
+-- 
+-- -# Fire Resistance #-
+-- @ Gives you resistance to fire 
+-- @ May be given by a potion
+--
+-- -# Illness #-
+-- @ Slowly removes your HP
+-- @ May be given randomly in some circumstances
+--
+-- -# Fatique #-
+-- @ Slows down your moviments and attacks
+-- @ May be given by a potion or running without resting/eating 
+
+import System.Console.ANSI
+import System.IO
+import System.Random 
+import Control.Monad
+import Data.Array
+
+class Attribute a where
+
+--Attribute's unique ID
+  id      :: a -> Int
+  
+--Attribute's unique name  
+  name    :: a -> String
+  
+--Attribute's Effects
+--effects :: [Effect]
--- Tile.hs
+++ Tile.hs
@@ -1,7 +1,31 @@
 module Tile where
+-- "TileType.Flammable"   for flammable tiles
+-- "TileType.Blessedness" for (bless/curse)able tiles
+-- "TileType.Worth"       for valuable tiles
 
 import System.Console.ANSI
 import System.IO
 import System.Random 
 import Control.Monad
 import Data.Array
+
+import qualified Attribute as A
+
+-- Basically, what can be Tile (e.g. Block, Item, Moster, Player, etc.)
+class TileType a where
+
+--Tile's unique ID
+  id     :: a -> Int
+
+--Tile's unique symbol
+  symbol :: a -> Char
+
+--Tile's unique name
+  name   :: a -> String
+
+-- Tile is the representation of each piece in a world, it has a type (Block, Item or Player) and
+-- their own Attributes (Attribute) that specify the aspects of a tile in the world, like the block
+-- damage or the intelligence level of a Player.
+data Tile a b where
+  Tile  :: (TileType a, A.Attribute b) => a -> [b] -> Tile a [b]
+  Above :: (TileType a, A.Attribute b) => Tile a [b] -> Tile a [b] -- One tile above the other
