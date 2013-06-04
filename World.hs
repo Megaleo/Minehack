43479bf20f95652435abc5b38682ee6e9856d7b2
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-04 16:44:30 -0300

    Removing world bounds and adding module commentaries

--- World.hs
+++ World.hs
@@ -1,8 +1,14 @@
 module World where
+--
+-- The way to handle "infinite" world elements is, in the nutshell, load 16x16 Tiles chunks with 
+-- random generation (seed) and verify if any of those have modified tiles.
+--
+-- This module will care of these world stuff, loading chunks mainly.
+
 
 import System.IO
 import Control.Monad
 import Data.Array
 
-worldBounds = ((1,1),(100,100))
+
 
