ced56f907f55f5d73e1d2da274c1ef60994861db
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-06 16:16:56 -0300

    Changing from roll to rollSeed

--- Random.hs
+++ Random.hs
@@ -4,8 +4,8 @@ import System.Random
 import Test.QuickCheck
 
 -- Convenient 'roll' dice \ random number generator
-roll :: Int -> Int -> Int -> Int -> Int
-roll min max determinant gen = round (toEnum(randomRs (min, (max * 100)) (mkStdGen gen) !! (determinant `mod` 100)) / 100)
+rollSeed :: Int -> Int -> Int -> Int -> Int
+rollSeed min max determinant seed = round (toEnum(randomRs (min, (max * 100)) (mkStdGen seed) !! (determinant `mod` 100)) / 100)
 
 -- Testing for how far it takes to a number repeat in a list 
 repPeriod :: Int -> [Int] -> Int
