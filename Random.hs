8b57f82735dda6cee45acae107f6b43d8dfe9ed5
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-06 19:07:10 -0300

    Roll type and changing "roll" to "rollSeed"

--- Random.hs
+++ Random.hs
@@ -1,11 +1,23 @@
 module Random where
 
+-- #@TODO@#
+--
+-- @Inplement support for other "Num" types in "Roll"
+
 import System.Random
 import Test.QuickCheck
 
 -- Convenient 'roll' dice \ random number generator
 rollSeed :: Int -> Int -> Int -> Int -> Int
 rollSeed min max determinant seed = round (toEnum(randomRs (min, (max * 100)) (mkStdGen seed) !! (determinant `mod` 100)) / 100)
+
+-- Roll min max seed or Fix number
+data Roll where
+  Roll :: Int -> Int -> Int -> Roll 
+  Fix  :: Int -> Roll 
+
+roll :: Roll -> Int
+roll (Roll min max seed) = rollSeed min max (seed `mod` 200) seed
 
 -- Testing for how far it takes to a number repeat in a list 
 repPeriod :: Int -> [Int] -> Int
@@ -17,7 +29,7 @@ repPeriod equal list = minimum . diffs . fst . unzip . filter equals $ zip [1..(
     diffs (x:xs) = [abs (head xs - x)] ++ diffs xs
     
 checkPeriod :: Int -> IO()
-checkPeriod min= quickCheck $ \y -> (repPeriod y $ map (\x -> roll 1 100 x 1615665) [1..1000]) > 50
+checkPeriod min= quickCheck $ \y -> (repPeriod y $ map (\x -> rollSeed 1 100 x 1615665) [1..1000]) > 50
 
 
 
