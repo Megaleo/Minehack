f3970c4e208941686a7135911c59825ab76191ab
Megaleo <leonardo.a.lessa@gmail.com>
2013-06-08 10:46:23 -0300

    tileChunk and type synomyms

--- World.hs
+++ World.hs
@@ -5,10 +5,23 @@ module World where
 --
 -- This module will care of these world stuff, loading chunks mainly.
 
-
 import System.IO
 import Control.Monad
 import Data.Array
 
+import qualified Tile as T
 
+-- type synonyms
+type Coord      = (Int, Int)
+type TileCoord  = Coord
+type ChunkCoord = Coord
+
+-- A chunk is 16x16 Tiles
+tileChunk :: TileCoord -> ChunkCoord
+tileChunk (x, y) = (new x, new y)
+  where
+    new var = floor $ (toEnum var) / 16 
+
+
+    
 
